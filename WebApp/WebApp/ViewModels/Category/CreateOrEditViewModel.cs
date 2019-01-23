6a2bb94f5bc7560e4a335ba65b1abc61c9f81bcf
Niyakonh Sisouphanh <Niya.90@hotmail.com>
2019-01-23 12:29:59 +0100

    Hehe en till

--- WebApp/WebApp/ViewModels/Category/CreateOrEditViewModel.cs
+++ WebApp/WebApp/ViewModels/Category/CreateOrEditViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,9 @@ namespace WebApp.ViewModels
 {
     public class CreateOrEditViewModel
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
 
     }
